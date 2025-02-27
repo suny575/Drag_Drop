f915651c733cc36ceac7dbee042c9820f16a36d1
suny575 <sunyfkr@gmail.com>
2025-02-27 05:03:56 -0800

    drop back to the left

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-let lists=document.getElementsByClassName("list");
-let rightbox=document.getElementById("right");
-let leftbox=document.getElementById("left");
+let lists = document.getElementsByClassName("list");
+let rightbox = document.getElementById("right");
+let leftbox = document.getElementById("left");
+let selected = "";
+
+for (let list of lists) {
+  list.addEventListen("dragstart", function (e) {
+    selected = e.target;
+
+    rightbox.addEventListener("dragover", function (e) {
+      e.preventDefault();
+    });
+
+    rightbox.addEventListener("drop", function (e) {
+      rightbox.appendChild(selected);
+      selected = null;
+    });
 
 
-
-for(let list of lists)
-{
-    list.addEventListen("dragstart",function(e){
-        let selected=e.target;
-
-        rightbox.addEventListener('dragover',function(e)
-    {
-        e.preventDefault();
-    })
-    })
+  });
 }
